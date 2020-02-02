a564d66766fee385b78f35d0dfdba6a5a7b3f64e
EskalonaD <eskalonad@gmail.com>
2020-02-02 13:24:55 +0200

    remove encapsulation from root comonent, remove bodynatural sytles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { StoreService } from './store.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
   constructor(private store: StoreService){}
