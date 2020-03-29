667666efea91473e1d283bf5ac86b93ab0be402d
EskalonaD <eskalonad@gmail.com>
2020-03-30 00:42:23 +0300

    add data models

---
+++ src/app/model.ts
@@ -0,0 +1,30 @@
+// Types of data storages for tasks output
+
+interface Task {
+    taskId: number;
+    delay: {
+      delayDuration: number;
+      baseDate: Date;
+    }
+  }
+  
+  interface TaskTimeSlot {
+    invokeIn: number; // string || date??
+    tasks: Task[];
+  }
+  
+  interface TaskByTime {
+    today: TaskTimeSlot[];
+    daily: TaskTimeSlot[];
+    weekly: TaskTimeSlot[];
+    monthly: TaskTimeSlot[];
+    yearly: TaskTimeSlot[];
+  }
+  
+  // Type for task storing
+  
+  interface TaskStorage {
+    tasksById: {
+      [key: number]: Task //not that task from tasks-output fix name collisions.
+    }
+  }
